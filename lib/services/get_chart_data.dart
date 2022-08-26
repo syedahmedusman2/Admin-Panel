2fcf1da311eeafa949f63993e5dcfd77e5a62956
Ahmed Usman <ahmedusmanpakistan@gmail.com>
2022-08-27 00:00:00 +0500

    update

--- lib/services/get_chart_data.dart
+++ lib/services/get_chart_data.dart
@@ -1,3 +1,7 @@
 import 'package:adminpanel/model/chart_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-Future<ChartModel> getChartData() async {}
+Future<ChartModel> getChartData() async {
+  CollectionReference _firestore =
+      FirebaseFirestore.instance.collection("users");
+}
