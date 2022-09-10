63b3c582124f9720c58d331906948ee522d89799
Ahmed Usman <ahmedusmanpakistan@gmail.com>
2022-09-11 00:23:40 +0500

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       routes: {
         '/home': (context) => HomePage(),
         '/login': (context) => LoginPage(),
